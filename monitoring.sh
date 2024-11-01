4a183d7544428a6df71775510717dd0b9abee759
Worakrit Kullanatpokin <worrakit.boss@gmail.com>
2024-11-01 17:12:58 +0700

    add new command

--- monitoring.sh
+++ monitoring.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Architecture
 	arc=$(uname -a)
 
@@ -6,17 +7,16 @@
 # vCPU
 	vcpu=$(grep "^processor" /proc/cpuinfo | wc -l)
 # CPU load
-	cpul=$(top -bn1 | grep '^%Cpu' | cut -c 9- | xargs | awk '{printf("%.1f%%"), $1 + $3}')
+	cpul=$(vmstat 1 2 | tail -1 | awk '{printf("%.1f%%"), 100 - $15}')
 
 # Memory Usage
 	fram=$(free -m | awk '$1 == "Mem:" {print $2}')
 	uram=$(free -m | awk '$1 == "Mem:" {print $3}')
 	pram=$(free | awk '$1 == "Mem:" {printf("%.2f"), $3/$2*100}')
-
 # Disk Usage
 	fdisk=$(df -Bg | grep '^/dev/' | grep -v '/boot$' | awk '{ft += $2} END {print ft}')
 	udisk=$(df -Bm | grep '^/dev/' | grep -v '/boot$' | awk '{ut += $3} END {print ut}')
-	pdisk=$(df -Bm | grep '^/dev/' | grep -v '/boot$' | awk '{utar += $3} {ft+= $2} END {printf("%d"), ut/ft*100}')
+	pdisk=$(df -Bm | grep '^/dev/' | grep -v '/boot$' | awk '{ut += $3} {ft+= $2} END {printf("%d"), ut/ft*100}')
 
 # Last boot
 	lb=$(who -b | awk '$1 == "system" {print $3 " " $4}')
@@ -24,7 +24,7 @@
 	lvmu=$(if [ $lvmt -eq 0 ]; then echo No; else echo Yes; fi)
 
 # Connections TCP
-	ctcp=$(cat /proc/net/sockstat{,6} | awk '$1 == "TCP:" {print $3}')
+	ctcp=$(ss -ta | grep ESTAB | wc -l)
 
 # User log
 	ulog=$(users | wc -w)
@@ -40,9 +40,9 @@
 wall "  #Architecture: $arc
 	#CPU physical: $pcpu
 	#vCPU: $vcpu
-	#CPU load: $udisk/${fdisk}Gb ($pdisk%)
+	#CPU load: $cpul
 	#Memory Usage: $uram/${fram}MB ($pram%)
-	#Disk Usage: $cpul
+	#Disk Usage: $udisk/${fdisk}Gb ($pdisk%)
 	#Last boot: $lb
 	#LVM use: $lvmu
 	#Connections TCP: $ctcp ESTABLISHED
